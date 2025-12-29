a681c91187db8a849009b1c818e13a18497eabc1
Karan Sardar <snsardarkaran61@gmail.com>
2025-12-29 12:06:17 +0530

    spring security web config -SecurityFilterChain Configuration

--- SecurityApplication/src/main/java/com/codingShuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
+++ SecurityApplication/src/main/java/com/codingShuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -1,4 +1,53 @@
 package com.codingShuttle.SecurityApp.SecurityApplication.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/posts").permitAll()
+                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(csrfconfig->csrfconfig.disable())
+                .sessionManagement(sessionConfig->sessionConfig
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService(){
+        UserDetails normaluserDetails= User
+                .withUsername("anuj")
+                .password(passwordEncoder().encode("anuj123"))
+                .roles("USER")
+                .build();
+        UserDetails adminUser=User
+                .withUsername("anuj_admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(normaluserDetails,adminUser);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
